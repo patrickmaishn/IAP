4088b08a5b9908ffa626c864c45d8e2fd91d301d
patrickmaishn <patricknmaina0@gmail.com>
2024-08-28 15:12:47 +0300

    Computer user

---
+++ flc.php
@@ -0,0 +1,21 @@
+<?php
+class fnc{
+    var $fname;
+    var $yob;
+    public $username;
+    protected $email_address;
+    private $password;
+
+    public function computer_user($fname){
+          return $fname;
+    }
+
+    public function user_age ($name, $yob){
+       $age = date('Y') - $yob;
+       return $name . " is " .$age;
+
+    }
+}
+
+$Obj = new fnc();
+print $Obj->user_age("Patrick", 1964);
