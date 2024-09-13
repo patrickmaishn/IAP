01fec3984c4fb51cf96ac031c7946b24040165c0
patrickmaishn <patricknmaina0@gmail.com>
2024-09-13 12:26:48 +0300

    dbs

--- includes/constants.php
+++ includes/constants.php
@@ -2,7 +2,7 @@
 //creating constants
 define('DBTYPE', 'MySQLi');
 define('HOSTNAME', 'localhost');
-define('DBPORT', '3307');
+define('DBPORT', '3306');
 define('HOSTUSER', 'root');
-define('HOSTPASS', '');
-define('DBNAME', '');
+define('HOSTPASS', 'Patrickmaina05$');
+define('DBNAME', 'iap_d');
