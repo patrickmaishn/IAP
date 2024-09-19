3d7f4edbf948d3785d0fe41d8f01f27c9f3c9b1e
patrickmaishn <patricknmaina0@gmail.com>
2024-09-19 21:09:51 +0300

    Bootstrap

--- contents/contents.php
+++ contents/contents.php
@@ -1,27 +1,40 @@
 <?php
 class contents{
     public function main_content(){
-        ?>
-        <div class="row">
-            <div class="content">
-                <h1>Main Content</h1>
-                <p>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.</p>
+?>
+    <div class="row align-items-md-stretch">
+        <div class="col-md-9">
+          <div class="h-100 p-5 text-bg-dark rounded-3">
+            <h2>Main Content</h2>
+            <p>Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium doloremque laudantium, totam rem aperiam, eaque ipsa quae ab illo inventore veritatis et quasi architecto beatae vitae dicta sunt explicabo. Nemo enim ipsam voluptatem quia voluptas sit aspernatur aut odit aut fugit, sed quia consequuntur magni dolores eos qui ratione voluptatem sequi nesciunt. Neque porro quisquam est, qui dolorem ipsum quia dolor sit amet, consectetur, adipisci velit, sed quia non numquam eius modi tempora incidunt ut labore et dolore magnam aliquam quaerat voluptatem. Ut enim ad minima veniam, quis nostrum exercitationem ullam corporis suscipit laboriosam, nisi ut aliquid ex ea commodi consequatur? Quis autem vel eum iure reprehenderit qui in ea voluptate velit esse quam nihil molestiae consequatur, vel illum qui dolorem eum fugiat quo voluptas nulla pariatur?</p>
+          </div>
+        </div>
 
-            </div>
-            <?php
+
+<?php
     }
+    public function about_content(){
+?>
+<div class="row">
+<div class="content">
+    <H1>About Content</H1>
+<p>Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium doloremque laudantium, totam rem aperiam, eaque ipsa quae ab illo inventore veritatis et quasi architecto beatae vitae dicta sunt explicabo. Nemo enim ipsam voluptatem quia voluptas sit aspernatur aut odit aut fugit, sed quia consequuntur magni dolores eos qui ratione voluptatem sequi nesciunt. Neque porro quisquam est, qui dolorem ipsum quia dolor sit amet, consectetur, adipisci velit, sed quia non numquam eius modi tempora incidunt ut labore et dolore magnam aliquam quaerat voluptatem. Ut enim ad minima veniam, quis nostrum exercitationem ullam corporis suscipit laboriosam, nisi ut aliquid ex ea commodi consequatur? Quis autem vel eum iure reprehenderit qui in ea voluptate velit esse quam nihil molestiae consequatur, vel illum qui dolorem eum fugiat quo voluptas nulla pariatur?</p>
+    </div>
+<?php
+    }
+
     public function side_bar(){
         ?>
-        <div class="sidebar">
-            <h1>Sidebar</h1>
-            <P>
-            It is a long established fact that a reader will be distracted by the readable content of a page when looking at its layout. The point of using Lorem Ipsum is that it has a more-or-less normal distribution of letters, as opposed to using 'Content here, content here', making it look like readable English. Many desktop publishing packages and web page editors now use Lorem Ipsum as their default model text, and a search for 'lorem ipsum' will uncover many web sites still in their infancy. Various versions have evolved over the years, sometimes by accident, sometimes on purpose (injected humour and the like).
-            </P>
+        <div class="col-md-3">
+          <div class="h-100 p-5 bg-body-tertiary border rounded-3">
+            <h2>Add borders</h2>
+            <p>Or, keep it light and add a border for some added definition to the boundaries of your content. Be sure to look under the hood at the source HTML here as we've adjusted the alignment and sizing of both column's content for equal-height.</p>
+            <button class="btn btn-outline-secondary" type="button">Example button</button>
+          </div>
+        </div>
+        
+      </div>
 
-           </div>
-
-       </div>
-
-     <?php
+        <?php
     }
 }
--- load.php
+++ load.php
@@ -1,38 +1,45 @@
-<?php 
+<?php
 
-//Class auto load.
+// Class Auto Load 
 
 function classAutoLoad($classname){
 
-$directories = ["contents", "layouts", "menus"];
+    $directories = ["contents", "layouts", "menus", "forms"];
 
-foreach($directories AS $dir){
-$filename = dirname(__FILE__) . DIRECTORY_SEPARATOR . $dir . DIRECTORY_SEPARATOR . $classname . ".php";
-if(file_exists($filename) AND is_readable($filename)){
-    require_once $filename;
-   }
-  }
+    foreach($directories AS $dir){
+        $filename = dirname(__FILE__) . DIRECTORY_SEPARATOR . $dir . DIRECTORY_SEPARATOR . $classname . ".php";
+        if(file_exists($filename) AND is_readable($filename)){
+            require_once $filename;
+        }
+    }
 }
 
 spl_autoload_register('classAutoLoad');
 
-
-$ObjLayouts = new layouts();
-$ObjMenus = new menus();
-$ObjHeadings = new headings();
-$ObjCont = new contents();
+// Create instances of all classes
+    $ObjLayouts = new layouts();
+    $ObjMenus = new menus();
+    $ObjHeadings = new headings();
+    $ObjCont = new contents();
+    $ObjForm = new user_forms();
 
 require "includes/constants.php";
-require "includes/dbconnection.php";
+require "includes/dbConnection.php";
 
-$conn = new dbconnection(DBTYPE, HOSTNAME, DBPORT, HOSTUSER, HOSTPASS, DBNAME);
+$conn = new dbConnection(DBTYPE, HOSTNAME, DBPORT, HOSTUSER, HOSTPASS, DBNAME);
 
-/*print dirname(__FILE__);
-echo "<br>";
-print $_SERVER["PHP_SELF"];
-echo "<br>";
-print basename($_SERVER["PHP_SELF"]);
-echo "<br>";
-if (file_exists("index.php") AND is_readable("index.php")){
-    print "yes";
-}*/
+
+// print 
+// print "<br>";
+// print "<br>";
+// print $_SERVER["PHP_SELF"];
+// print "<br>";
+// print "<br>";
+// print basename($_SERVER["PHP_SELF"]);
+// print "<br>";
+// print "<br>";
+// if(file_exists("index.php") AND is_readable("index.php")){
+//     print "yes";
+// }else{
+//     print "no";
+// }
