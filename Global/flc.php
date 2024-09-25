a24bd4542881091013a91d87c2993bc29b4f768c
patrickmaishn <patricknmaina0@gmail.com>
2024-09-25 16:41:10 +0300

    Created function to return input validation feedback

---
+++ Global/flc.php
@@ -0,0 +1,17 @@
+<?php
+class Flc{
+    public function setMsg($name, $values, $class){
+          if(is_array($values)){
+            $_SESSION[$name] = $values;
+          }else{
+            $_SESSION[$name] = '<span class="' .$class. '">' .$values . '</span>';
+          }
+    }
+
+    public function getMsg($name){
+       if (isset($_SESSION[$name])){
+        $session = $_SESSION[$name];
+        return $session;
+       }
+    }
+}
